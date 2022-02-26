548c628eda4b7e450d145d17a7d0773f139c2850
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2022-02-26 16:00:22 -0500

    Create a mecanum module (#2)
    Co-Authored by: @Amoskeag

--- arduino/robot/robot.ino
+++ arduino/robot/robot.ino
@@ -4,6 +4,12 @@
 // Config/Build
 #include "version.h"
 
+// Custom objects
+#include "modules/mecanum_module.cpp"
+
+Mecanum front_left_mecanum(1, 2, 3, 4);
+Mecanum front_right_mecanum(5, 6, 7, 8, true);
+// More modules, exercise to the reader ;)
 
 void setup()
 {
@@ -11,12 +17,23 @@ void setup()
   Serial.begin(115200);
 
   // Print out some information about the software we're running.
-  Serial.println(); Serial.print("Starting robot software. Using version "); Serial.println(VERSION);
-  Serial.print("This software compiled on "); Serial.println(COMPILED_ON); Serial.println();
+  Serial.println();
+  Serial.print("Starting robot software. Using version ");
+  Serial.println(VERSION);
+  Serial.print("This software compiled on ");
+  Serial.println(COMPILED_ON);
+  Serial.println();
   delay(500); // This is ok to use here
 }
 
 void loop()
 {
-  delay(100); //TODO: Write code here
+  front_left_mecanum.setSpeed(255);
+  delay(1000); // Dont do dis irl lol
+  front_left_mecanum.setSpeed(0);
+  delay(1000); // Dont do dis irl lol
+  front_left_mecanum.setSpeed(-255);
+  delay(1000); // Dont do dis irl lol
+  front_left_mecanum.setSpeed(0);
+  delay(1000); // Dont do dis irl lol
 }
