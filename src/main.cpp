49bc574c5d9c0f287f2331da1081246cb75fa613
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2022-03-13 21:49:17 -0400

    Remove makefile mess (#6)

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,21 @@
 #include <Arduino.h>
 #include "mecanum.h"
 
-
 Mecanum front_left_mecanum(1, 2, 3, 4);
 Mecanum front_right_mecanum(5, 6, 7, 8, true);
-//Mecanum back_left_mecanum(x, )
-// More modules, exercise to the reader ;)
+// Mecanum back_left_mecanum(x, )
+//  More modules, exercise to the reader ;)
 
 void setup()
 {
-  // Init serial
+  // Setup Serial
   Serial.begin(115200);
 
-  // Print out some information about the software we're running.
-  Serial.println();
-  Serial.print("Starting robot software. Using version ");
-  //Serial.println(VERSION);
-  Serial.print("This software compiled on ");
-  // Serial.println(COMPILED_ON);
-  Serial.println();
+  Serial.println("Configured Serial Interfaces.");
+  Serial.println("This software expects your terminal to be \x1B[46mVT100 Compatable\x1B[0m.");
+
+  // Print MOTD
+  Serial.println(MOTD);
   delay(500);
 }
 
